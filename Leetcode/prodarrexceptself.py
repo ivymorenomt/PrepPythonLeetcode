052b8069cbad4ebe78b446931ef0db875c9168e0
Marjorie T. Ivy <mivy8@gatech.edu>
2022-02-19 14:18:29 -0600

    Modified Leetcode Notes

---
+++ Leetcode/prodarrexceptself.py
@@ -0,0 +1,13 @@
+'''
+ Given an integer array, return array answer such that answer[i] is equal to the product of all the elements
+ 
+ ex: [1,2,3,4]
+ output [24, 12, 8, 6]
+ 
+ where - for 1, it should be 2*3*4 = 24
+ 2 = 1*3*4 = 12
+ ..and so on
+ Don't solve with division 
+ 
+ 
+'''
